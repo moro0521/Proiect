adba980a7ebeb0bac4afdbc91042510db155d574
AndreiCovei <andrey_fuzzyboy@yahoo.com>
2016-01-27 09:42:29 +0200

    ConnectionString si Gridview
    ConnectionString si Gridview

--- activitatiSportive/StartPage.aspx.cs
+++ activitatiSportive/StartPage.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,14 @@ namespace activitatiSportive
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            SqlDataReader reader;
+            SqlConnection conn = new SqlConnection("Data Source=ARKWRIGHT-PC;Initial Catalog=ActivitatiSportive;Integrated Security=True");
+            conn.Open();
+            string query = "SELECT * FROM StudentiParticipanti";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            reader = cmd.ExecuteReader();
+            GridView1.DataSource = reader;
+            GridView1.DataBind();
         }
     }
 }
